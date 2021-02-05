4bcdf068d9015593f2cbe3a4b80ce8dfe0b3d2a7
Dmitry Usik <d.usik@riseapps.biz>
2021-02-05 09:57:36 +0200

    - refactored html parser;

--- src/html/htmlUtils.ts
+++ src/html/htmlUtils.ts
@@ -2,14 +2,18 @@ import fs from 'fs';
 import parse from 'node-html-parser';
 
 import { topicsUtils } from '../shared';
-import { InterviewQuestions, Question, QuestionData, Level } from '../types';
+import { InterviewQuestions, Level, Question, QuestionData } from '../types';
 import { config, questionsDBTopics } from '../wrappers';
 
 const questionDataSeparator = ' [';
 const questionExtraData = ']';
-const ignoredRows = ['Links', 'http'];
+const logicalAnd = '&amp;&amp;';
+const mainContentKey = '#main-content';
 
-const getTopicsIndexes = (content: string[]) =>
+const replaceLogicalAnd = (question: string): string =>
+  question.includes(logicalAnd) ? question.replace(logicalAnd, '&&') : question;
+
+const getDBTopicsIndexes = (content: string[]): number[] =>
   Object.keys(questionsDBTopics).map(topic => {
     let lastIndexOf = -1;
 
@@ -34,7 +38,7 @@ const parseDatabaseQuestion = (
     key: `${topicsUtils.topicToKey(questionsDBTopics[topic])}${questionsLength + 1}`,
     estimatedTimeMin: Number.parseFloat(questionSplit[2].replace(questionExtraData, '')),
     requiredFor: questionSplit[1].replace(questionExtraData, '') as Level,
-    question: questionSplit[0],
+    question: replaceLogicalAnd(questionSplit[0]),
   };
 };
 
@@ -42,27 +46,30 @@ export const parseQuestionsDB = (): InterviewQuestions => {
   const html = fs.readFileSync(config.questionsDatabasePath, 'utf8');
 
   const parsedHtml = parse(html);
-  const mainContent = parsedHtml.querySelector('#main-content');
+  const mainContent = parsedHtml.querySelector(mainContentKey);
   const mainContentSplit = mainContent.structuredText.split('\n');
-  const topicsIndexes = getTopicsIndexes(mainContentSplit);
+  const dbTopicsIndexes = getDBTopicsIndexes(mainContentSplit);
 
   const interviewQuestions: InterviewQuestions = {};
 
-  topicsIndexes.forEach((topicIndex, index) => {
-    const topic = Object.keys(questionsDBTopics).find(innerTopic =>
-      mainContentSplit[topicIndex].includes(innerTopic)
+  dbTopicsIndexes.forEach((dbTopicIndex, index) => {
+    const topic = Object.keys(questionsDBTopics).find(dbTopic =>
+      mainContentSplit[dbTopicIndex].includes(dbTopic)
     );
     const question: Question = {
       data: [],
     };
 
-    const until =
-      index === topicsIndexes.length - 1
-        ? mainContentSplit.length - 1
-        : topicsIndexes[topicsIndexes.findIndex(innerIndex => innerIndex === topicIndex) + 1];
+    const lastDBTopicsItemIndex = dbTopicsIndexes.length - 1;
+    const currentDBTopicsIndex = dbTopicsIndexes.findIndex(
+      possibleIndex => possibleIndex === dbTopicIndex
+    );
+    const nextDBTopicsIndex = dbTopicsIndexes[currentDBTopicsIndex + 1];
+    const lastContentItemIndex = mainContentSplit.length - 1;
+    const until = index === lastDBTopicsItemIndex ? lastContentItemIndex : nextDBTopicsIndex;
 
-    for (let i = topicIndex + 1; i < until; i++) {
-      if (mainContentSplit[i] !== ignoredRows[0] && !mainContentSplit[i].includes(ignoredRows[1])) {
+    for (let i = dbTopicIndex + 1; i < until; i++) {
+      if (mainContentSplit[i] !== 'Links' && !mainContentSplit[i].includes('http')) {
         question.data.push(parseDatabaseQuestion(mainContentSplit[i], question.data.length, topic));
       }
     }
